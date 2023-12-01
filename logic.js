b79433edce96d21a420648234c71f0fbf7e53697
nonebula <147166151+nonebula@users.noreply.github.com>
2023-12-01 15:13:22 +0000

    Added questions to js & spread across two js files, deleted script.js

---
+++ logic.js
@@ -0,0 +1,23 @@
+//1. Create questions x10 [within JS]
+
+//2. on click quiz starts
+
+//3. when quiz starts, timer starts
+
+//4. box options need to appear for each question
+
+//5. If wrong 'incorrect' shows up at bottom, if correct 'correct' shows at bottom.
+
+//6. If wrong user gets -10 secs. If correct user gets +10 secs
+
+//7. correct answers need to be counted to count score
+
+//8. end of quiz enter initials
+
+//9. highscores and username needs to be stored
+
+//10. clear highscores should clear list
+
+//11. 
+
+//12. 
---
+++ questions.js
@@ -0,0 +1,61 @@
+
+//Questions: 
+// What is the purpose of the "document.getElementById" method in JavaScript?
+// a. Retrieve an element by its class
+// b. Retrieve an element by its tag name
+// c. Retrieve an element by its ID *
+// d. Retrieve an element by its name
+
+// Which keyword is used to declare variables in JavaScript?
+// a. var *
+// b. int
+// c. let
+// d. const
+
+// What does the "=== " operator do in JavaScript?
+// a. Checks for equality without type conversion *
+// b. Checks for equality with type conversion
+// c. Assigns a value to a variable
+// d. Checks for inequality without type conversion
+
+// What does the "querySelector" method do in JavaScript?
+// a. Selects the first element that matches a specified CSS selector *
+// b. Selects all elements that match a specified CSS selector
+// c. Selects an element by its ID
+// d. Selects an element by its class
+
+// How do you comment out multiple lines in JavaScript?
+// a. // This is a comment *
+// b. / This is a comment /
+// c. <!-- This is a comment -->
+// d. # This is a comment
+
+// Which function is used to convert a string to an integer in JavaScript?
+// a. toInteger()
+// b. parseInt() *
+// c. convertToInt()
+// d. strToNum()
+
+// What is the purpose of the "addEventListener" method in JavaScript?
+// a. Adds a new element to the DOM
+// b. Attaches an event handler to an element *
+// c. Removes an event listener from an element
+// d. Adds a style to an element
+
+// What is the role of the "this" keyword in JavaScript?
+// a. Refers to the current function
+// b. Refers to the global object
+// c. Refers to the current object *
+// d. Declares a new variable
+
+// What is the purpose of the "JSON.stringify" method in JavaScript?
+// a. Converts a JavaScript object to a JSON string *
+// b. Parses a JSON string to a JavaScript object
+// c. Adds a property to a JavaScript object
+// d. Removes a property from a JavaScript object
+
+// What does the "Promise" object in JavaScript represent?
+// a. A value that may be available now, later, or never
+// b. A synchronous operation
+// c. A one-time event in the future
+// d. A loop that continues indefinitely *
